d1bdfeb7b289ecd4363bee5a7b7528e25fb5cc9f
Aman KUmar <amankumar.spj410@gmail.com>
2023-09-17 14:43:18 +0530

    added todo routes and weather

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import { expressjwt } from 'express-jwt';
 import router from './routes/index.js';
 
-dotenv.config();
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 
-app.use('/api', router);
+app.use(expressjwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256'],
+    credentialsRequired: true,
+    getToken: (req) => {
+        if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer')
+            return req.headers.authorization.split(' ')[1]
+        else if (req.query && req.query.token)
+            return req.query.token
+        return null
+    },
+    onExpired: (req, res) => res.status(401).json({ message: 'Token expired' }),
+}).unless({ path: ['/api/login', '/api/register'] }))
 
-const port = process.env.PORT || 8080;
+app.use('/api', router)
+
+const port = parseInt(process.env.PORT) || 8080;
 
 app.listen(port, () => {
-    console.log('Example app listening on port ' + port + '! ')
+    console.log('Example app listening on port http://localhost:' + port + ' ! ')
 })
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ import prisma from '../database/index.js';
 const router = express.Router();
 
 // create all routes here
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
 router.put('/register', async (req, res) => {
     try {
@@ -14,8 +15,6 @@ router.put('/register', async (req, res) => {
         if (!email || !password || !name) 
             throw { code: 400, message: 'Missing required fields: username, password or name' }
 
-        // email regex
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email))
             throw { code: 400, message: 'Invalid email address' }
         if (password.length < 6) 
@@ -63,8 +62,6 @@ router.post('/login', async (req, res) => {
         if (!email || !password ) 
             throw { code: 400, message: 'Missing required fields: username or password' }
 
-        // email regex
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         if (!emailRegex.test(email))
             throw { code: 400, message: 'Invalid email address' }
         if (password.length < 6) 
@@ -78,7 +75,7 @@ router.post('/login', async (req, res) => {
         })
 
         // check if user exists
-        if (!user.uid)
+        if (!user || !user.uid)
             throw { code: 400, message: 'User does not exist' }
 
         // check if password is correct
@@ -96,7 +93,7 @@ router.post('/login', async (req, res) => {
         )
 
         // send token to client
-        res.status(201).json({ token, message: 'User created successfully' })
+        res.status(200).json({ token, message: 'User created successfully' })
     } catch (error) {
         console.log(error)
         res
@@ -105,5 +102,150 @@ router.post('/login', async (req, res) => {
     }
 })
 
-export default router;
+// create note
+router.put('/todo', async (req, res) => {
+    try {
+        const { body, data = {}, at = new Date() } = req.body
+        const { uid } = req.auth
+        
+        if (!body)
+            throw { code: 400, message: 'Missing required fields: body' }
 
+        if (data && typeof data !== 'object')
+            throw { code: 400, message: 'Data must be an object' }
+
+        const todo = await prisma.todos.create({
+            data: {
+                body,
+                data,
+                at: new Date(at),
+                uid
+            }
+        })
+
+        res.status(201).json({ todo, message: 'Todo created successfully' })
+    } catch (error) {
+        console.log(error)
+        res
+            .status(error.code && error.code < 501 ? error.code : 500)
+            .json({ message: error.message || "Internal Server Error" })
+    }
+})
+
+// get todo
+router.get('/todo/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { uid } = req.auth
+
+        if (!id)
+            throw { code: 400, message: 'Missing required fields: id' }
+
+        // check if todon exists with id and uid
+        const todo = await prisma.todos.findFirst({
+            where: {
+                id,
+                uid
+            }
+        })
+
+        if (!todo)
+            throw { code: 404, message: 'Todo does not exist' }
+
+        res.status(200).json({ todo, message: 'Todo retrieved successfully' })
+    } catch (error) {
+                console.log(error)
+                res
+                    .status(error.code && error.code < 501 ? error.code : 500)
+                    .json({ message: error.message || "Internal Server Error" })
+    }
+
+})
+
+// get todos by date range
+router.get('/todo/:from/:to/:skip/:take', async (req, res) => {
+    try {
+        // from and to are unix timestamps
+        const { from, to, skip = 0, take = 10 } = req.params
+        const { uid } = req.auth
+
+        console.log(from, to, skip, take)
+        if (!from || !to)
+            throw { code: 400, message: 'Missing required fields: from or to' }
+
+        // check if todon exists with id and uid
+        const todos = await prisma.todos.findMany({
+            where: {
+                uid,
+                at: {
+                    gte: new Date(parseInt(from)),
+                    lte: new Date(parseInt(to))
+                }
+            },
+            orderBy: {
+                at: 'asc'
+            },
+            skip: parseInt(skip),
+            take: parseInt(take)
+        })
+
+        res.status(200).json({ todos, message: 'Todos retrieved successfully' })
+
+    } catch (error) {
+        console.log(error)
+        res
+            .status(error.code && error.code < 501 ? error.code : 500)
+            .json({ message: error.message || "Internal Server Error" })
+    }
+})
+
+router.delete('/todo/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { uid } = req.auth
+
+        if (!id)
+            throw { code: 400, message: 'Missing required fields: id' }
+
+        // check if todon exists with id and uid
+        const todo = await prisma.todos.deleteMany({
+            where: {
+                AND: {
+                    id,
+                    uid
+                }
+            }
+        })
+
+        if (!todo)
+            throw { code: 404, message: 'Todo does not exist' }
+
+        res.status(200).json({ todo, message: 'Todo deleted successfully' })
+
+    } catch (error) {
+        console.log(error)
+        res
+            .status(error.code && error.code < 501 ? error.code : 500)
+            .json({ message: error.message || "Internal Server Error" })
+    }
+})
+
+router.get('/weather/:lat/:lon', async (req, res) => {
+    try {
+        const { lat, lon } = req.params
+
+        if (!lat || !lon)
+            throw { code: 400, message: 'Missing required fields: lat or lon' }
+
+        const weather = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${process.env.WEATHER_API_KEY}`)
+            .then(res => res.json())
+
+        res.status(200).json({ weather, message: 'Weather retrieved successfully' })
+    } catch (error) {
+        console.log(error)
+        res
+            .status(error.code && error.code < 501 ? error.code : 500)
+            .json({ message: error.message || "Internal Server Error" })
+    }
+})
+export default router;
